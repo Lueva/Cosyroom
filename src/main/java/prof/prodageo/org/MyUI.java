b1d4c052c3f70b0d256e390d2d4ecc7099dc1890
Enora GICQUEL <enora.gicquel@insa-rouen.fr>
2016-05-20 14:55:04 +0200

    avancement ihm

--- src/main/java/prof/prodageo/org/MyUI.java
+++ src/main/java/prof/prodageo/org/MyUI.java
@@ -7,11 +7,7 @@ import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
 
 
 import org.slf4j.Logger;
@@ -22,10 +18,10 @@ import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.Button.ClickEvent;
 
 /**
- * This UI is the application entry point. A UI may either represent a browser window 
+ * This UI is the application entry point. A UI may either represent a browser window
  * (or tab) or some part of a html page where a Vaadin application is embedded.
  * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be
  * overridden to add component to the user interface and initialize non-component functionality.
  */
 @Theme("mytheme")
@@ -36,48 +32,65 @@ public class MyUI extends UI {
 
     /* explicit declaration as attributes of graphical components for GenMyModel */
         final VerticalLayout layout = new VerticalLayout();
-        final TextField name = new TextField();
-        final TextField surname = new TextField();
-        Button button = new Button("Click Me") ;
-        
+        final HorizontalLayout layoutRecherche = new HorizontalLayout();
+        final VerticalLayout layoutAnnonces = new VerticalLayout();
+        final TextField lieu = new TextField();
+        final PopupDateField dateDepart = new PopupDateField();
+        final PopupDateField dateFin = new PopupDateField();
+        final Slider prixMin = new Slider();
+        final Slider prixMax = new Slider();
+        Button button = new Button("Rechercher") ;
+
 
     /* explicit callback */
     /* https://vaadin.com/docs/-/part/framework/application/application-events.html */
     public class ClickMeClass implements Button.ClickListener
     {
-        public void buttonClick(ClickEvent event) 
+        public void buttonClick(ClickEvent event)
         {
-            layout.addComponent(new Label("Thanks " + name.getValue() + ", it works!"));
-            log.info("Button clicked with value : " + name.getValue());
+            //layout.addComponent(new Label("Thanks " + name.getValue() + ", it works!"));
+            //log.info("Button clicked with value : " + name.getValue());
         }
     }
-        
+
 
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
 
-        
+
         // final VerticalLayout layout = new VerticalLayout();
-        
+
         // final TextField name = new TextField();
-        name.setCaption("Type your name here:");
+        //name.setCaption("Type your name here:");
 
         /*
         Button button = new Button("Click Me");
         button.addClickListener( e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
+            layout.addComponent(new Label("Thanks " + name.getValue()
                     + ", it works!"));
             log.info("Button clicked with value : " + name.getValue());
         });
         */
+
+        prixMin.setImmediate(true);
+        prixMin.setMin(0.0);
+        prixMin.setMax(1000.0);
+        prixMin.setValue(0.0);
+
+        prixMax.setImmediate(true);
+        prixMax.setMin(0.0);
+        prixMax.setMax(1000.0);
+        prixMax.setValue(500.0);
+
         ClickMeClass callback = new ClickMeClass() ;
         button.addClickListener( callback ) ;
+        layoutRecherche.addComponents(lieu, dateDepart, dateFin);
+        layout.addComponents(layoutRecherche, layoutAnnonces);
 
-        layout.addComponents(name, button);
         layout.setMargin(true);
         layout.setSpacing(true);
-        
+
         setContent(layout);
     }
 
